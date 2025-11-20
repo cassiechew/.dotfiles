90b1f438d22ee6e1d0267e356790225ec2571161
Cassie Chew <me@cassie.id>
2025-11-21 09:54:37 +1100

    adds: gitsigns

--- private_dot_config/nvim/lua/cassie/lazy/gitsigns.lua
+++ private_dot_config/nvim/lua/cassie/lazy/gitsigns.lua
@@ -1,4 +1,15 @@
 return {
     "lewis6991/gitsigns.nvim",
-    opts = {}
+    opts = {
+        signs = {
+            add             = { text = "+"},
+            change          = { text = "~"},
+            untracked       = { text = "?"},
+        },
+        signs_staged = {
+            add             = { text = "+"},
+            change          = { text = "~"},
+            untracked       = { text = "?"},
+        }
+    }
 }
