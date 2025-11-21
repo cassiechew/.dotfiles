f1813b62acf9601a8e63a51d88302eaff11afb78
Cassie Chew <me@cassie.id>
2025-11-21 23:43:24 +1100

    adds config for diff-view

--- private_dot_config/nvim/lua/cassie/lazy/diff-view.lua
+++ private_dot_config/nvim/lua/cassie/lazy/diff-view.lua
@@ -1,5 +1,52 @@
 return {
-    "sindrets/diffview.nvim",
-    opts = {}
-}
+	"sindrets/diffview.nvim",
+	config = function()
+		require("diffview").setup({
+			enhanced_diff_hl = true,
+			use_iconns = true,
+			view = {
+				default = {
+					layout = "diff2_horizontal", -- side-by-side (good for wide monitors)
+				},
+				merge_tool = {
+					layout = "diff3_horizontal", -- 3-way merge layout preference
+				},
+			},
+			keymaps = {
+				view = {
+					["q"] = "<cmd>DiffviewClose<CR>",
+					["<leader>e"] = "<cmd>DiffviewToggleFiles<CR>",
+					["<tab>"] = "<cmd>DiffviewToggleFiles<CR>",
+					["gf"] = "<cmd>DiffviewFocusFiles<CR>",
+					["<C-n>"] = "select_next_entry",
+					["<C-p>"] = "select_prev_entry",
+				},
+				file_panel = {
+					["q"] = "<cmd>DiffviewClose<CR>",
+					["<cr>"] = "select_entry",
+					["<tab>"] = "toggle_stage_entry",
+					["s"] = "stage_all",
+					["u"] = "unstage_all",
+					["j"] = "next_entry",
+					["k"] = "prev_entry",
+				},
+			},
+		})
 
+		local map = vim.keymap.set
+
+		-- Open diff vs HEAD
+		map("n", "<leader>gd", "<cmd>DiffviewOpen<CR>", { desc = "Diffview: HEAD" })
+
+		-- Diff against current branch’s upstream
+		map("n", "<leader>gu", "<cmd>DiffviewOpen @{upstream}<CR>", { desc = "Diffview: upstream" })
+
+		-- View file history for current file
+		map("n", "<leader>gh", function()
+			vim.cmd("DiffviewFileHistory " .. vim.fn.expand("%"))
+		end, { desc = "Diffview: file history" })
+
+		-- Toggle files panel when already in diffview
+		map("n", "<leader>gf", "<cmd>DiffviewToggleFiles<CR>", { desc = "Diffview: toggle files panel" })
+	end,
+}
