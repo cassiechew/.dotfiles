ae921ba807cfdff0144a15f062e6c16a6e32b481
Cassie Chew <me@cassie.id>
2025-11-22 00:18:48 +1100

    Updates fugitive keybinds

--- private_dot_config/nvim/lua/cassie/lazy/fugitive.lua
+++ private_dot_config/nvim/lua/cassie/lazy/fugitive.lua
@@ -1,6 +1,40 @@
 return {
 	"tpope/vim-fugitive",
 	config = function()
-		vim.keymap.set("n", "<leader>gs", vim.cmd.Git, {desc = "Open fugitive"})
-	end
+		local map = vim.keymap.set
+
+		-- Fugitive status
+		map("n", "<leader>gs", function()
+			vim.cmd.Git()
+		end, { desc = "Fugitive: Status" })
+
+		-- Stage file
+		map("n", "<leader>ga", function()
+			vim.cmd.Gwrite()
+		end, { desc = "Fugitive: Stage file" })
+
+		-- Unstage file
+		map("n", "<leader>gA", function()
+			vim.cmd("Git restore --staged " .. vim.fn.expand("%"))
+		end, { desc = "Fugitive: Unstage file" })
+
+		-- Blame
+		map("n", "<leader>gb", function()
+			vim.cmd.Git("blame")
+		end, { desc = "Fugitive: Blame" })
+
+		-- Commit
+		map("n", "<leader>gc", function()
+			vim.cmd.Git("commit")
+		end, { desc = "Fugitive: Commit" })
+
+		-- Push / Pull
+		map("n", "<leader>gp", function()
+			vim.cmd.Git("push")
+		end, { desc = "Fugitive: Push" })
+
+		map("n", "<leader>gP", function()
+			vim.cmd.Git("pull")
+		end, { desc = "Fugitive: Pull" })
+	end,
 }
